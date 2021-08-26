d1c3d2f9ca498d035c5135073d6bf4dcd973d170
catha <catharinaczipf@hotmail.com>
2021-08-26 14:05:23 +0200

    I hate this soooo much

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,15 @@
 const UserData = require("../model/userModel");
 const express = require("express");
 
-// Middleware – show all Users
+// Middleware – get the One User ✔️
+const getOneUser = async (req, res) => {
+  res.status(200).json(res.user);
+};
+
+// Middleware – show all Users ✔️
 const getAllUser = async (req, res) => {
   try {
     const user = await UserData.find();
-    // 200 for Successful Ok
     res.status(200).json(
       user.map((user) => {
         return {
@@ -25,7 +29,7 @@ const getAllUser = async (req, res) => {
   }
 };
 
-// Middleware – add new User to DataBase
+// Middleware – add new User to DataBase ✔️
 const addNewUser = async (req, res) => {
   const user = new UserData({
     userName: req.body.userName,
@@ -37,28 +41,27 @@ const addNewUser = async (req, res) => {
   });
 
   // EXAMPLE:
-  // {
-  //   "userName": "Catha",
-  //   "userPass": "123pass",
-  //   "age": 32,
-  //   "fbw": 48,
-  //   "toolStack": ["Js", "Html5", "Css3", "Sass"],
-  //   "eMail": "contact@cath.at"
-  // }
+  //   {
+  //     "userName": "Catha",
+  //     "userPass": "123pass",
+  //     "age": 32,
+  //     "fbw": 48,
+  //     "toolStack": ["Js", "Html5", "Css3", "Sass"],
+  //     "eMail": "contact@cath.at"
+  //   }
   try {
-    // save
     const newUser = await user.save();
     // 201 for Successful Created
     res.status(201).json(`I just added: ${newUser}`);
   } catch (err) {
-    // 400 for Bad request
+    // 400 Bad request
     res.status(400).json({
       message: err.message,
     });
   }
 };
 
-// Middleware – get User by name
+// Middleware – get User by name NOT WORKING SO FAR
 const getUser = async (req, res, next) => {
   let user;
   try {
@@ -78,8 +81,56 @@ const getUser = async (req, res, next) => {
   next();
 };
 
+// Middleware – Update (whole) User by name (PUT)
+const updateUser = async (req, res) => {
+  try {
+    await UserData.updateOne(
+      { name: req.params.userName },
+      {
+        $set: {
+          userName: req.body.userName,
+          userPass: req.body.userPass,
+          age: req.body.age,
+          fbw: req.body.fbw,
+          eMail: req.body.eMail,
+          toolStack: req.body.toolStack,
+        },
+      }
+    );
+    // 200 OK
+    res.status(200).json({ message: "User is updated" });
+  } catch (err) {
+    res.status(400).json({ message: "WTF" });
+  }
+};
+
+// Middleware –  Update parts of an User by name (PATCH)
+const updateUserPart = async (req, res) => {
+  const { userName, userPass, age, fbw, eMail, toolStack } = req.body;
+  if (userName) {
+    res.user.userName = userName;
+    res.user.userPass = userPass;
+    res.user.age = age;
+    res.user.fbw = fbw;
+    res.user.eMail = eMail;
+    res.user.toolStack = toolStack;
+  }
+  try {
+    await res.user.save();
+    res
+      .status(200)
+      .json({ message: "User received an Update", data: res.employee });
+  } catch (err) {
+    // 400 Bad request
+    res.status(400).json({ message: err.message });
+  }
+};
+
 module.exports = {
+  getOneUser,
   getUser,
   getAllUser,
   addNewUser,
+  updateUser,
+  updateUserPart,
 };
--- router/user.js
+++ router/user.js
@@ -3,12 +3,19 @@ const router = express.Router();
 const UserData = require("../model/userModel");
 const {
   getUser,
+  getOneUser,
   getAllUser,
   addNewUser,
+  updateUser,
+  updateUserPart,
 } = require("../controllers/userController");
 
 router.route("/").get(getAllUser).post(addNewUser);
 
-router.route("/:userName").get(getUser);
+router
+  .route("/:userName")
+  .get(getUser, getOneUser)
+  .put(getUser, updateUser)
+  .patch(getUser, updateUserPart);
 
 module.exports = router;
